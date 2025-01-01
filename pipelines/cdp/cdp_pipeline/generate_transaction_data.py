9bc95ffa7c0a6bcef7fc66d390811e4c23efc012
Israel Herraiz <ihr@google.com>
2025-01-01 18:52:34 +0100

    Update copyright headers

--- pipelines/cdp/cdp_pipeline/generate_transaction_data.py
+++ pipelines/cdp/cdp_pipeline/generate_transaction_data.py
@@ -1,3 +1,17 @@
+#  Copyright 2025 Google LLC
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 from google.cloud import storage
 from google.cloud import pubsub_v1
 import csv
@@ -15,7 +29,7 @@ async def publish_coupons_to_pubsub():
     transactions_data = '<path to transactions data in gcs bucket>' # reference example - 'dataflow-solution-guide-cdp/input_data/transaction_data.csv'
     coupons_topic_name = "coupon_redemption"
     coupons_data = '<path to coupon redemption data in gcs bucket>' # reference example - 'dataflow-solution-guide-cdp/input_data/coupon_redempt.csv'
-                                                                     
+
     transactions_df = pd.read_csv(f"gs://{bucket_name}/{transactions_data}",dtype=str)
     coupons_df = pd.read_csv(f"gs://{bucket_name}/{coupons_data}",dtype=str)
     publisher = pubsub_v1.PublisherClient() 
