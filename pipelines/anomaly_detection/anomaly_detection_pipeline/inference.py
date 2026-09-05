5e0480af19080fc290bfb8aebbef301a1e5c2959
Israel Herraiz <isra@herraiz.org>
2026-09-05 19:45:59 +0200

    chore: add Google LLC Apache 2.0 copyright headers to all pipeline, terraform, script, and container files

--- pipelines/anomaly_detection/anomaly_detection_pipeline/inference.py
+++ pipelines/anomaly_detection/anomaly_detection_pipeline/inference.py
@@ -1,3 +1,16 @@
+#  Copyright 2026 Google LLC
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
 """Batched Vertex AI prediction with bounded retries and strict association."""
 from apache_beam.ml.inference.base import PredictionResult, RemoteModelHandler
 from google.api_core.exceptions import DeadlineExceeded, ServerError, TooManyRequests
